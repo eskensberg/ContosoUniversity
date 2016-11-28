70500a2a85a8ce458cb61bc486034a9d4af26bfa
Elvis Skensberg <eskensberg@gmail.com>
2016-11-28 14:46:48 +0000

    Random Changes

---
+++ UniversityBooking/ViewModels/RoomBookingData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityBooking.Models;
+
+namespace UniversityBooking.ViewModels
+{
+    public class RoomBookingData
+    {
+        public IEnumerable<Room> Rooms { get; set; }
+        public IEnumerable<BookingRecord> BookingRecords { get; set; }
+    }
+}
